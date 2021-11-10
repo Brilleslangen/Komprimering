c61dc212f80d820cf8b4a42f7fea9dd46198448f
Nicolai Sivesind <nicolts@stud.ntnu.no>
2021-11-10 20:54:01 +0100

    reset

---
+++ src/LZ77.java
@@ -0,0 +1,196 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class LZ77 {
+    private static final int WINDOW_SIZE = Byte.MAX_VALUE, BUFFER_SIZE = Byte.MAX_VALUE;
+    private static final String testWord = "a aasqzzz ax xzywplm";
+    private static final byte[] bytes = testWord.getBytes();
+
+
+    public static void main(String[] args) {
+        translateTest();
+    }
+
+    private static void opg8() {
+        String filename = "lorem.txt";
+        String compFilename = "compressed-" + filename.split("\\.")[0] + ".Z";
+        String uncompFilename = "uncompressed-" + filename;
+
+        // Compress
+        byte[] compressed = compress(readFile(filename));
+        writeCompressedFile(compressed, compFilename);
+
+        // Decompress
+        byte[] decompressed = decompress(readCompressedFile(compFilename));
+        writeFile(decompressed, uncompFilename);
+    }
+
+    private static void readWriteTest() {
+        byte[] compressed = compress(bytes);
+        writeCompressedFile(compressed, "test.Z");
+
+        // Decompress
+        byte[] decompressed = decompress(readCompressedFile("test.Z"));
+        writeFile(decompressed, "test.txt");
+    }
+
+    private static void translateTest() {
+        byte[] compressed = compress(bytes);
+        System.out.println("Word to compress : \n" + testWord + "\n");
+        System.out.println("Compressed: ");
+        for (byte b : compressed) {
+            System.out.print(b + " ");
+        }
+
+        System.out.println("\nDecompressed: ");
+        byte[] decompressed = decompress(compressed);
+        for (byte b : decompressed) {
+            System.out.print((char) b);
+        }
+    }
+
+    private static byte[] readCompressedFile(String filename) {
+        byte[] bytes;
+        try (DataInputStream dis = new DataInputStream((new BufferedInputStream(new FileInputStream(System.getProperty("user.dir") + "/" + filename))))) {
+            bytes = dis.readAllBytes();
+            dis.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("An error occured while writing file.");
+        }
+        return bytes;
+    }
+
+    private static void writeCompressedFile(byte[] data, String filename) {
+        try (DataOutputStream dos = new DataOutputStream(
+                new BufferedOutputStream(new FileOutputStream(System.getProperty("user.dir") + "/" + filename)))) {
+
+            for (byte b : data) {
+                dos.writeByte(b);
+            }
+            dos.flush();
+            dos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("An error occured while writing file.");
+        }
+    }
+
+    private static byte[] readFile(String filename) {
+        try {
+            return Files.readAllBytes(Paths.get(System.getProperty("user.dir") + "/" + filename));
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("An error occured while loading file.");
+        }
+    }
+
+    private static void writeFile(byte[] data, String filename) {
+        File file = new File(System.getProperty("user.dir") + "/" + filename);
+        try {
+            Files.write(file.toPath(), data);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("An error occured while writing file.");
+        }
+    }
+
+
+    private static byte[] decompress(byte[] encodedStream) {
+        ArrayList<Byte> decodedStream = new ArrayList<>();
+
+        for (int position = 0; position < encodedStream.length; position++) {
+            // if uncompressed
+            if (encodedStream[position] < 0) {
+                for (int i = 0; i < Math.abs(encodedStream[position - i]); i++) {
+                    position++;
+                    decodedStream.add(encodedStream[position]);
+                }
+                continue;
+            }
+
+            // if sequence reference
+            int offset = encodedStream[position];
+            int length = encodedStream[position + 1];
+            position++;
+
+            for (int l = 0; l < length; l++) {
+                decodedStream.add(decodedStream.get(decodedStream.size() - offset));
+            }
+        }
+        return unwrap(decodedStream);
+    }
+
+    private static byte[] compress(byte[] input) {
+        ArrayList<Byte> output = new ArrayList<>();
+        int noMatchCount = 0;
+
+        // Setting pointer to start of stream
+        for (int position = 0; position < input.length;) {
+            // Finding the longest common byte pattern for window and buffer
+            int pointerLength = 0;
+            int pointerOffset = 0;
+            boolean matchFound = false;
+
+            for (int peekLength = BUFFER_SIZE; peekLength > 0; peekLength--) {
+                for (int offset = 0; offset <= WINDOW_SIZE - peekLength; offset++) {
+                    int peekEnd = Math.min(position + peekLength, input.length);
+                    byte[] peek = Arrays.copyOfRange(input, position, peekEnd);
+                    int windowStart = Math.max(position - WINDOW_SIZE + offset, 0);
+                    int windowEnd = Math.max(position - WINDOW_SIZE + offset + peekLength, 0);
+                    byte[] window = Arrays.copyOfRange(input, windowStart, windowEnd);
+
+                    // If a match is found, save the pointer
+                    if (Arrays.equals(peek, window) && peek.length > 2) {
+                        pointerOffset = position - windowStart;
+                        pointerLength = peek.length;
+                        position += peek.length;
+                        matchFound = true;
+                        break;
+                    }
+                }
+                if (matchFound)
+                    break;
+            }
+
+            boolean isLastEntry = position >= input.length-1;
+            boolean isMaxedOut = noMatchCount == Byte.MIN_VALUE;
+
+            // if a repeating sequence is found or last element of the input or uncompressed index maxed out
+            if (matchFound || isLastEntry || isMaxedOut) {
+                if (isLastEntry && !matchFound) {
+                    noMatchCount++;
+                    pointerLength++;
+                }
+                if (noMatchCount > 0 || isMaxedOut) {
+                    output.add((byte) -noMatchCount);
+                    for (int i = noMatchCount + pointerLength; i > pointerLength; i--) {
+                        output.add(input[position - i]);
+                    }
+                    noMatchCount = 0;
+                }
+                if (matchFound) {
+                    // Add matched sequence
+                    output.add((byte) pointerOffset);
+                    output.add((byte) pointerLength);
+                    continue;
+                }
+            }
+            // If no match
+            position++;
+            noMatchCount++;
+        }
+        return unwrap(output);
+    }
+
+    private static byte[] unwrap(ArrayList<Byte> bigBytes) {
+        byte[] bytes = new byte[bigBytes.size()];
+        for (int i = 0; i < bytes.length; i++) {
+            bytes[i] = bigBytes.get(i);
+        }
+        return bytes;
+    }
+}
